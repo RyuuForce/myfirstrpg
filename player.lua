3ca57ac2dadf4f0da8a3b74c738c95959328eb1f
RyuuForce <ryuforce2@gmail.com>
2023-09-23 12:11:14 +0200

    updated and reordered some sprites

--- player.lua
+++ player.lua
@@ -6,37 +6,43 @@ player.x = 8
 player.y = 8
 player.dx = 1
 player.dy = 1
-player.direction = 1
-player.sprite = {1,2,3,4}
+player.spriteindex = 1
+player.sprites = {1,2,3,4,5,6,7}
 player.weapon = {17,18,19,20}
+player.weaponSpriteIndex = 1
+player.weaponAnimationTimer = 0
 player.draw = function(self)
-    spr(self.sprite[self.direction], self.x * grid_size, self.y * grid_size)	
+    spr(self.sprites[self.spriteindex], self.x * grid_size, self.y * grid_size)	
 end
 player.move = function(self)
-    if btn(0) then
-        self.x -= 1
-	self.direction = 1
-    elseif btn(1) then
-	self.x += 1
-	self.direction = 2
-    elseif btn(2) then
-	self.y -= 1
-	self.direction = 3
-    elseif btn(3) then
-	self.y += 1
-	self.direction = 4
-    end
+  if btn(0) then
+    self.x -= 1
+	  self.spriteindex = 1
+    self.weaponSpriteIndex = 1
+  elseif btn(1) then
+	  self.x += 1
+	  self.spriteindex = 2
+    self.weaponSpriteIndex = 2
+  elseif btn(2) then
+	  self.y -= 1
+	  self.spriteindex = 3
+    self.weaponSpriteIndex = 3
+  elseif btn(3) then
+	  self.y += 1
+	  self.spriteindex = 4
+    self.weaponSpriteIndex = 4
+  end
 end
 player.attack = function(self)
-    if btn(4) then
-	if self.direction == 1 then
-	    spr(self.weapon[self.direction], (self.x - 8), self.y * grid_size)
-	elseif self.direction == 2 then
-	    spr(self.weapon[self.direction], (self.x + 8), self.y * grid_size)
-	elseif self.direction == 3 then
-	    spr(self.weapon[self.direction], self.x, (self.y - 8) * grid_size)
-	elseif self.direction == 4 then
-	    spr(self.weapon[self.direction], self.x, (self.y + 8) * grid_size)
-	end
-    end
+  if btn(4) then
+  	if self.spriteindex == 1 then
+	    spr(self.weapon[self.weaponSpriteIndex], (self.x - 8), self.y * grid_size)
+	  elseif self.spriteindex == 2 then
+	    spr(self.weapon[self.weaponSpriteIndex], (self.x + 8), self.y * grid_size)
+	  elseif self.spriteindex == 3 then
+	    spr(self.weapon[self.weaponSpriteIndex], self.x, (self.y - 8) * grid_size)
+	  elseif self.spriteindex == 4 then
+	    spr(self.weapon[self.weaponSpriteIndex], self.x, (self.y + 8) * grid_size)
+	  end
+  end
 end
